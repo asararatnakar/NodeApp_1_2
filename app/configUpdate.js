6d452d5e823e3f246d4c6976dee3434cb81a0f13
asararatnakar <asara.ratnakar@gmail.com>
2018-07-05 11:28:20 -0400

    collections_config.json correction + formatting
    Signed-off-by: asararatnakar <asara.ratnakar@gmail.com>

--- app/configUpdate.js
+++ app/configUpdate.js
@@ -10,7 +10,7 @@ const agent = require('superagent-promise')(require('superagent'), Promise);
 var helper = require('./helper.js');
 var logger = helper.getLogger('Create-Channel');
 //Attempt to send a request to the orderer with the sendTransaction method
-var configUpdate = async function(channelName, username, orgName, body) {
+var configUpdate = async function (channelName, username, orgName, body) {
 	logger.debug('\n====== Creating Channel \'' + channelName + '\' ======\n');
 	try {
 		// first setup the client for this org
@@ -18,9 +18,9 @@ var configUpdate = async function(channelName, username, orgName, body) {
 		logger.debug('Successfully got the fabric client for the organization "%s"', orgName);
 
 		// enable Client TLS
-		var tlsInfo =  await helper.tlsEnroll(client);
+		var tlsInfo = await helper.tlsEnroll(client);
 		client.setTlsClientCertAndKey(tlsInfo.certificate, tlsInfo.key);
-        //TODO: Check what type of config update ?
+		//TODO: Check what type of config update ?
 		//TODO: needs to include the following + Cleanup
 		// 1. don't hardcode the URLs and PATHs
 		// 2. Read MSPIDS dynamically
@@ -29,44 +29,43 @@ var configUpdate = async function(channelName, username, orgName, body) {
 		const configJson = JSON.parse(channelCfg.toString());
 		configJson.channel_id = channelName;
 
-        let mspPlaceHolder = {
-            "policies": {
-                "Admins": {},
-                "Readers": {},
-                "Writers": {}
-            },
-            "values": {
-                "MSP": {}
-            }
-        };
-        let anchorPlaceHolder = {
-                "mod_policy": "Admins",
-                "values": {
-                    "AnchorPeers": {
-                        "mod_policy": "Admins",
-                        "value": {
-                            "anchor_peers": [
-                                {
-                                    "host": "peer",
-                                    "port": 7051
-                                }
-                            ]
-                        },
-                        "version": "0"
-                    },
-                    "MSP": {}
-                },
-                "version": "1"
-            };
-            configJson.read_set.groups.Application.groups[client.getMspid()] = mspPlaceHolder;
-            anchorPlaceHolder.policies = mspPlaceHolder.policies;
-            anchorPlaceHolder.values.AnchorPeers.value.anchor_peers[0].host = body.host;
-            anchorPlaceHolder.values.AnchorPeers.value.anchor_peers[0].port = body.port;
-            configJson.write_set.groups.Application.groups[client.getMspid()] = anchorPlaceHolder;
-            // console.log(JSON.stringify(configJson));
-            const config = await agent.post('http://127.0.0.1:7059/protolator/encode/common.ConfigUpdate', JSON.stringify(configJson)).buffer();
-            let channelConfig = config.body;
-			// console.log(channelConfig.toString());
+		let mspPlaceHolder = {
+			"policies": {
+				"Admins": {},
+				"Readers": {},
+				"Writers": {}
+			},
+			"values": {
+				"MSP": {}
+			}
+		};
+		let anchorPlaceHolder = {
+			"mod_policy": "Admins",
+			"values": {
+				"AnchorPeers": {
+					"mod_policy": "Admins",
+					"value": {
+						"anchor_peers": [
+							{
+								"host": "peer",
+								"port": 7051
+							}
+						]
+					},
+					"version": "0"
+				},
+				"MSP": {}
+			},
+			"version": "1"
+		};
+		configJson.read_set.groups.Application.groups[client.getMspid()] = mspPlaceHolder;
+		anchorPlaceHolder.policies = mspPlaceHolder.policies;
+		anchorPlaceHolder.values.AnchorPeers.value.anchor_peers[0].host = body.host;
+		anchorPlaceHolder.values.AnchorPeers.value.anchor_peers[0].port = body.port;
+		configJson.write_set.groups.Application.groups[client.getMspid()] = anchorPlaceHolder;
+		const config = await agent.post('http://127.0.0.1:7059/protolator/encode/common.ConfigUpdate', JSON.stringify(configJson)).buffer();
+		let channelConfig = config.body;
+		// console.log(channelConfig.toString());
 
 		//Acting as a client in the given organization provided with "orgName" param
 		// sign the channel config bytes as "endorsement", this is required by
@@ -97,7 +96,7 @@ var configUpdate = async function(channelName, username, orgName, body) {
 			throw new Error('Failed to update the channel \'' + channelName + '\' with Anchor peer');
 		}
 	} catch (err) {
-		logger.error('Failed to initialize the channel: ' + err.stack ? err.stack :	err);
+		logger.error('Failed to initialize the channel: ' + err.stack ? err.stack : err);
 		throw new Error('Failed to initialize the channel: ' + err.toString());
 	}
 };
